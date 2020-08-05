879dcf2e15f6a8e0476137b205b5a9372da70796
howuvebeenkeeping <rigabdullin@yandex.ru>
2020-08-05 15:00:01 +0500

    Add planet video

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var instance = new vidbg('.video', {
+    webm: 'video/world.webm', // URL or relative path to webm video
+    poster: 'video/poster.jpg', // URL or relative path to fallback image
+    overlay: false, // Boolean to display the overlay or not
+});
